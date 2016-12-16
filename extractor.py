3bb5608bbe0740c0f09ddbe4be12fc8e80d3d38a
Silas Pinho Ladislau <splhead@gmail.com>
2016-12-16 13:07:50 -0400

    update do extrator

--- extractor.py
+++ extractor.py
@@ -15,13 +15,23 @@ def dateFormat(date):
 	dia = numbers.findall(date)
 
 	for mes in meses:
+		sMes = ""
+		sDia = ""
 		if date.__contains__(mes) :
-			out = str(dia[0]) + "/" + str(meses.index(mes) + 1)
-			return out
+			if meses.index(mes) + 1 < 10 :
+				sMes = '0' + str(meses.index(mes) + 1)
+			else:
+				sMes = str(meses.index(mes) + 1)
+
+			if int(dia[0]) < 10 :
+				sDia = '0' + str(dia[0])
+			else:
+				sDia = str(dia[0])
+			return "2016-" + sMes + "-" + sDia
 
 os.chdir(".")
 
-json_obj = "["
+json_obj = '['
 
 for file in sorted(glob.glob("*.xhtml"),key=numericalSort):
 	#print(file)
@@ -42,10 +52,12 @@ for file in sorted(glob.glob("*.xhtml"),key=numericalSort):
 	for paragrafo in paragrafos:
 		texto += paragrafo.text + "\n\n"
 
-	texto += "'},"
+	texto += "',"
 	json_obj += texto
+	json_obj += "'type':" + str(4) + "},"
 
-json_obj += "]"
+json_obj  = json_obj[:-1]
+json_obj += ']'
 out = json.dumps(json_obj)
 
 print out
