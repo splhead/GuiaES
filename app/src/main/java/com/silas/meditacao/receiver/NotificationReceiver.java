5239d7dad3a92cfefc3c76251144d692ae79166c
splhead <splhead@gmail.com>
2018-06-29 21:41:07 -0400

    fix notification's title

--- app/src/main/java/com/silas/meditacao/receiver/NotificationReceiver.java
+++ app/src/main/java/com/silas/meditacao/receiver/NotificationReceiver.java
@@ -20,45 +20,49 @@ import java.util.Calendar;
 public class NotificationReceiver extends BroadcastReceiver {
     public final String CHANNEL_ID = "com.silas.meditacao";
     private Context mContext;
+
     public NotificationReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         mContext = context;
-        String[] msg = getGreeting();
-        String notificationText = getVerseFromPreferedDevotional();
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context, CHANNEL_ID)
-                        .setSmallIcon(getNotificationIcon())
-                        .setContentTitle(msg[0])
-                        .setContentText(notificationText)
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(notificationText));
+        Meditacao meditacao = getTodayPreferedDevotional();
+        if (meditacao != null) {
+            String title = meditacao.getTitulo();
+            String text = meditacao.getTextoBiblico();
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context, CHANNEL_ID)
+                            .setSmallIcon(getNotificationIcon())
+                            .setContentTitle(title)
+                            .setContentText(text)
+                            .setStyle(new NotificationCompat.BigTextStyle()
+                                    .bigText(text));
 
-        Intent resultIntent = new Intent(context, MainActivity.class);
+            Intent resultIntent = new Intent(context, MainActivity.class);
 
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
-                        context,
-                        0,
-                        resultIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
+            PendingIntent resultPendingIntent =
+                    PendingIntent.getActivity(
+                            context,
+                            0,
+                            resultIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
 
-        mBuilder.setContentIntent(resultPendingIntent);
+            mBuilder.setContentIntent(resultPendingIntent);
 
-        Notification n = mBuilder.build();
-        n.flags = Notification.FLAG_AUTO_CANCEL;
+            Notification n = mBuilder.build();
+            n.flags = Notification.FLAG_AUTO_CANCEL;
 
-        // Sets an ID for the notification
-        int mNotificationId = 1;
-        // Gets an instance of the NotificationManager service
-        NotificationManager mNotifyMgr =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        // Builds the notification and issues it.
-        if (mNotifyMgr != null) {
-            mNotifyMgr.notify(mNotificationId, n);
+            // Sets an ID for the notification
+            int mNotificationId = 1;
+            // Gets an instance of the NotificationManager service
+            NotificationManager mNotifyMgr =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            // Builds the notification and issues it.
+            if (mNotifyMgr != null) {
+                mNotifyMgr.notify(mNotificationId, n);
+            }
         }
     }
 
@@ -67,27 +71,23 @@ public class NotificationReceiver extends BroadcastReceiver {
         return useWhiteIcon ? R.drawable.ic_notification : R.mipmap.ic_launcher;
     }
 
-    private String[] getGreeting() {
+    /*private Pair<String, String> getGreeting() {
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
 
         if (hour < 13) {
-            return new String[]{"Bom dia!", "Que tal começar o dia bem!"};
+            return new Pair<>("Bom dia!", "Que tal começar o dia bem!");
         } else if (hour < 19) {
-            return new String[]{"Boa tarde!", "Que bom que você reservou um tempo para mim!"};
+            return new Pair<>("Boa tarde!", "Que bom que você reservou um tempo para mim!");
         } else {
-            return new String[]{"Boa noite!", "Como foi seu dia? Agora vai ficar melhor!"};
+            return new Pair<>("Boa noite!", "Como foi seu dia? Agora vai ficar melhor!");
         }
-    }
+    }*/
 
-    private String getVerseFromPreferedDevotional() {
+    private Meditacao getTodayPreferedDevotional() {
         Calendar today = Calendar.getInstance();
         MeditacaoDBAdapter meditacaoDBAdapter = new MeditacaoDBAdapter(mContext);
-        Meditacao meditacao = meditacaoDBAdapter.buscaMeditacao(today, getPreferedDevotionalType());
-        if( meditacao != null) {
-            return meditacao.getTextoBiblico();
-        }
-        return getGreeting()[1];
+        return meditacaoDBAdapter.buscaMeditacao(today, getPreferedDevotionalType());
     }
 
     private int getPreferedDevotionalType() {
