73eedfa69314c8dd41dbbb30a11f0a716d684828
splhead <splhead@gmail.com>
2018-10-29 22:05:21 -0400

    checkpoint native ads

--- app/src/main/java/com/silas/meditacao/activity/FavoritesActivity.kt
+++ app/src/main/java/com/silas/meditacao/activity/FavoritesActivity.kt
@@ -3,6 +3,12 @@ package com.silas.meditacao.activity
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdLoader
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.formats.UnifiedNativeAd
 import com.silas.guiaes.activity.R
 import com.silas.meditacao.adapters.FavoritesListAdapter
 import com.silas.meditacao.adapters.MeditacaoDBAdapter
@@ -10,23 +16,70 @@ import com.silas.meditacao.models.Meditacao
 import kotlinx.android.synthetic.main.favorites_activity.*
 import java.lang.ref.WeakReference
 
+
 class FavoritesActivity : ThemedActivity() {
+    companion object {
+        const val NUMBER_OF_ADS = 5
+    }
+
+    private lateinit var adLoader: AdLoader
+    private val mNativeAds: ArrayList<UnifiedNativeAd> = ArrayList()
     private lateinit var favAdapter: FavoritesListAdapter
+    private var mFavoritesItems: ArrayList<Any> = ArrayList()
     private var devotionals = ArrayList<Meditacao>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.favorites_activity)
 
-        fetchFavoriteTask(this).execute()
+        MobileAds.initialize(this, getString(R.string.admob_app_id))
+        FetchFavoriteTask(this).execute()
 
+    }
+
+    fun insertAdsInFavoritesItems() {
+        if (mNativeAds.size <= 0) {
+            return
+        }
+
+        val offset = (mFavoritesItems.size / mNativeAds.size) + 1
+        var index = 0
+        mNativeAds.forEach { ad ->
+            mFavoritesItems.add(index, ad)
+            index += offset
+        }
+    }
+
+    fun loadNativeAds() {
+        val builder = AdLoader.Builder(this, getString(R.string.ad_unit_id))
+        adLoader = builder.forUnifiedNativeAd { unifiedNativeAd ->
+            // A native ad loaded successfully, check if the ad loader has finished loading
+            // and if so, insert the ads into the list.
+            mNativeAds.add(unifiedNativeAd)
+            if (!adLoader.isLoading) {
+                insertAdsInFavoritesItems()
+            }
+        }.withAdListener(
+                object : AdListener() {
+                    override fun onAdFailedToLoad(errorCode: Int) {
+                        // A native ad failed to load, check if the ad loader has finished loading
+                        // and if so, insert the ads into the list.
+                        Log.e("FavoritesActivity", "The previous native ad failed to load. Attempting to" + " load another.")
+                        if (!adLoader.isLoading) {
+                            insertAdsInFavoritesItems()
+                        }
+                    }
+                }).build()
+
+        // Load the Native Express ad.
+        adLoader.loadAds(AdRequest.Builder().build(), NUMBER_OF_ADS)
     }
 
     fun setDevotional(devotionals: ArrayList<Meditacao>) {
         this.devotionals.clear()
         this.devotionals = devotionals
         val recyclerView = favorites_recycler_view
-        favAdapter = FavoritesListAdapter(devotionals, this)
+        favAdapter = FavoritesListAdapter(devotionals)
         recyclerView.adapter = favAdapter
 
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -34,7 +87,7 @@ class FavoritesActivity : ThemedActivity() {
     }
 
 
-    private class fetchFavoriteTask internal constructor(favoritesActivity: FavoritesActivity) : AsyncTask<Void, Void, ArrayList<Meditacao>>() {
+    private class FetchFavoriteTask internal constructor(favoritesActivity: FavoritesActivity) : AsyncTask<Void, Void, ArrayList<Meditacao>>() {
 
         private val wr: WeakReference<FavoritesActivity> = WeakReference(favoritesActivity)
 
@@ -46,6 +99,7 @@ class FavoritesActivity : ThemedActivity() {
         override fun onPostExecute(meditacoes: ArrayList<Meditacao>) {
             super.onPostExecute(meditacoes)
             wr.get()?.setDevotional(meditacoes)
+            wr.get()?.loadNativeAds()
         }
     }
 }
--- app/src/main/java/com/silas/meditacao/adapters/FavoritesListAdapter.kt
+++ app/src/main/java/com/silas/meditacao/adapters/FavoritesListAdapter.kt
@@ -1,40 +1,125 @@
 package com.silas.meditacao.adapters
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import com.google.android.gms.ads.formats.UnifiedNativeAd
+import com.google.android.gms.ads.formats.UnifiedNativeAdView
 import com.silas.guiaes.activity.R
 import com.silas.meditacao.models.Meditacao
 import kotlinx.android.synthetic.main.favorites_item.view.*
 
-class FavoritesListAdapter(private val devotionals: List<Meditacao>,
-                           private val context: Context) : RecyclerView.Adapter<FavoritesListAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.favorites_item
-                , parent, false)
-        return ViewHolder(view)
+class FavoritesListAdapter(private val favoriteViewItems: List<Any>) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    companion object {
+        private const val DEVOTIONAL_ITEM_VIEW_TYPE = 0
+        private const val UNIFIED_NATIVE_AD_VIEW_TYPE = 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val favoriteViewItem = favoriteViewItems[position]
+        if (favoriteViewItem is UnifiedNativeAd) {
+            return UNIFIED_NATIVE_AD_VIEW_TYPE
+        }
+        return DEVOTIONAL_ITEM_VIEW_TYPE
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            UNIFIED_NATIVE_AD_VIEW_TYPE -> {
+                val unifiedNativeLayoutView = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.ad_unified, parent, false)
+                return UnifiedAdViewHolder(unifiedNativeLayoutView)
+            }
+            else -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.favorites_item
+                        , parent, false)
+                return ViewHolder(view)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        return devotionals.size
+        return favoriteViewItems.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val devotional = devotionals[position]
-        holder.bindView(devotional)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val viewType = getItemViewType(position)
+        when (viewType) {
+            UNIFIED_NATIVE_AD_VIEW_TYPE -> {
+                val nativeAd = favoriteViewItems[position] as UnifiedNativeAd
+                (holder as UnifiedAdViewHolder)
+                populateNativeAdView(nativeAd, holder.adView)
+
+            }
+            else -> {
+                (holder as ViewHolder).bindView(favoriteViewItems[position] as? Meditacao)
+            }
+        }
+
+    }
+
+    fun populateNativeAdView(nativeAd: UnifiedNativeAd, adview: UnifiedNativeAdView) {
+        (adview.headlineView as TextView).text = nativeAd.headline
+        (adview.bodyView as TextView).text = nativeAd.body
+        (adview.callToActionView as TextView).text = nativeAd.callToAction
+
+        val icon = nativeAd.icon
+
+        if (icon == null) {
+            adview.iconView.visibility = View.GONE
+        } else {
+            (adview.iconView as ImageView).setImageDrawable(icon.drawable)
+            adview.iconView.visibility = View.VISIBLE
+        }
+
+        if (nativeAd.price == null) {
+            adview.priceView.visibility = View.GONE
+        } else {
+            adview.priceView.visibility = View.VISIBLE
+            (adview.priceView as TextView).text = nativeAd.price
+        }
+
+        if (nativeAd.store == null) {
+            adview.storeView.visibility = View.GONE
+        } else {
+            adview.storeView.visibility = View.VISIBLE
+            (adview.storeView as TextView).text = nativeAd.store
+        }
+
+        if (nativeAd.starRating == null) {
+            adview.starRatingView.visibility = View.GONE
+        } else {
+            (adview.starRatingView as RatingBar).rating = nativeAd.starRating.toFloat()
+            adview.starRatingView.visibility = View.VISIBLE
+        }
+
+        if (nativeAd.advertiser == null) {
+            adview.advertiserView.visibility = View.GONE
+        } else {
+            (adview.advertiserView as TextView).text = nativeAd.advertiser
+            adview.advertiserView.visibility = View.VISIBLE
+        }
+
+        adview.setNativeAd(nativeAd)
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
 
-        fun bindView(devotional: Meditacao) {
+        fun bindView(devotional: Meditacao?) {
             val title = itemView.tvTitle
             val verse = itemView.tvVerse
 
-            title.text = devotional.titulo
-            verse.text = devotional.textoBiblico
+            devotional?.let {
+                title.text = it.titulo
+                verse.text = it.textoBiblico
+            }
+
         }
     }
 }
