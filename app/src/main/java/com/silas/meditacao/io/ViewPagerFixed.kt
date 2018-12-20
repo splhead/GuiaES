56a69be7bb42bebb60527f4c210c9729b2fbf61e
Silas Pinho Ladislau <silaspinho@sefin.ro.gov.br>
2018-12-20 07:49:04 -0400

    fix bug illegalArgumentExecption

---
+++ app/src/main/java/com/silas/meditacao/io/ViewPagerFixed.kt
@@ -0,0 +1,36 @@
+package com.silas.meditacao.io
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.v4.view.ViewPager
+
+import android.util.AttributeSet
+import android.view.MotionEvent
+
+class ViewPagerFixed : ViewPager {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        try {
+            return super.onTouchEvent(ev)
+        } catch (ex: IllegalArgumentException) {
+            ex.printStackTrace()
+        }
+
+        return false
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        try {
+            return super.onInterceptTouchEvent(ev)
+        } catch (ex: IllegalArgumentException) {
+            ex.printStackTrace()
+        }
+
+        return false
+    }
+}
